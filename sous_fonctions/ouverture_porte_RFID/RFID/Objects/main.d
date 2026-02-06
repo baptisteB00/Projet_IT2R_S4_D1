8b728e4f12268714dca65d1c013412090fa6a4a9
Nohajas <noam.bellahcene@gmail.com>
2026-02-06 12:40:29 +0100

    Add files via upload

---
+++ sous_fonctions/ouverture_porte_RFID/RFID/Objects/main.d
@@ -0,0 +1,9 @@
+./objects/main.o: main.c \
+  C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\LPC17xx.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\system_LPC17xx.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.3.0\CMSIS\Driver\Include\Driver_USART.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.3.0\CMSIS\Driver\Include\Driver_Common.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Boards\Keil\MCB1700\Common\GLCD_Config.h \
+  C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.17.0\Board\Board_GLCD.h
---
+++ sous_fonctions/ouverture_porte_RFID/RFID/main.c
@@ -0,0 +1,58 @@
+#include "LPC17xx.h"                    // Device header
+#include "Driver_USART.h"               // CMSIS Driver:USART
+#include "RTE_Components.h"             // Component selection
+#include "GLCD_Config.h"                // Board Support:Graphic LCD
+#include "Board_GLCD.h"                 // Board Support:Graphic LCD
+#include "stdio.h"
+
+
+
+extern ARM_DRIVER_USART Driver_USART1;
+extern GLCD_FONT GLCD_Font_6x8;
+extern GLCD_FONT GLCD_Font_16x24;
+
+void Identification(unsigned char badge[], unsigned char recu[]);
+
+void Init_UART(void){ 
+	Driver_USART1.Initialize(NULL); 
+	Driver_USART1.PowerControl(ARM_POWER_FULL); 
+	Driver_USART1.Control( ARM_USART_MODE_ASYNCHRONOUS | 
+	ARM_USART_DATA_BITS_8 | 
+	ARM_USART_STOP_BITS_1 | 
+	ARM_USART_PARITY_NONE | 
+	ARM_USART_FLOW_CONTROL_NONE, 
+	115200); 
+	Driver_USART1.Control(ARM_USART_CONTROL_TX,1); 
+	Driver_USART1.Control(ARM_USART_CONTROL_RX,1); 
+}
+
+
+int main(){
+	unsigned char chaine_lue[10];
+	unsigned char badge[10] = {0,0,0,5,0,8,4,6,2,6};
+	
+	Init_UART();
+	GLCD_Initialize();
+	GLCD_ClearScreen();
+	GLCD_SetFont(&GLCD_Font_16x24);
+	
+	while (1){
+		Driver_USART1.Receive(chaine_lue,10);
+		Identification(chaine_lue, badge);
+	}	
+	return 0;
+}
+
+void Identification(unsigned char tab[], unsigned char recu[]){
+	int i, b = 0;
+	uint8_t texte[24];
+	for(i=0;i<10;i++){
+		if (tab[i] == recu[i]) b++;
+	}
+	if (b == 10){
+		sprintf(texte,"Badge reconnu");
+	}
+	else{
+		sprintf(texte,"Badge non reconnu");
+	}
+}
