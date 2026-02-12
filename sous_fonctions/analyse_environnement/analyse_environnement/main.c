63a27d622b04fd08158cfea546e5831107d7a507
Bryan <houybryan77@gmail.com>
2026-02-12 11:20:04 +0100

    Configuration du moteur Lidar

--- sous_fonctions/analyse_environnement/analyse_environnement/main.c
+++ sous_fonctions/analyse_environnement/analyse_environnement/main.c
@@ -1,5 +1,15 @@
-// Test GitHub
+#include "LPC17xx.h"                    // Device header
+#include "Driver_USART.h"               // CMSIS Driver:USART
 
-// Test 2 
-
-// Test 3
+int main()
+{
+	LPC_GPIO2->FIODIR0 |= 0x20; // Configuration de la broche P2.5 en sortie pour le moteur du Lidar
+	
+	while(1)
+	{
+		//LPC_GPIO2->FIOPIN0 &= 0xD0;	// Eteint Moteur Lidar 
+		LPC_GPIO2->FIOPIN0 |= 0x20;	// Allume Moteur Lidar 
+	}
+	
+	return 0; 
+}
