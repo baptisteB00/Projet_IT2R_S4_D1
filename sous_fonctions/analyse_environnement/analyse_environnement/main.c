e5974d71897dc6ec06c403c1c9454617ac58feec
Bryan <houybryan77@gmail.com>
2026-02-12 11:41:25 +0100

    Création de la fonction initialisation de l'UART pour le Lidar A2M6

--- sous_fonctions/analyse_environnement/analyse_environnement/main.c
+++ sous_fonctions/analyse_environnement/analyse_environnement/main.c
@@ -1,6 +1,13 @@
 #include "LPC17xx.h"                    // Device header
 #include "Driver_USART.h"               // CMSIS Driver:USART
 
+extern ARM_DRIVER_USART Driver_USART1;
+
+/*-------------- Prototypes des fonctions --------------*/
+
+void Init_UART_Lidar(void);
+
+
 int main()
 {
 	LPC_GPIO2->FIODIR0 |= 0x20; // Configuration de la broche P2.5 en sortie pour le moteur du Lidar
@@ -13,3 +20,26 @@ int main()
 	
 	return 0; 
 }
+
+void Init_UART_Lidar(void)
+{
+	Driver_USART1.Initialize(NULL);			
+	Driver_USART1.PowerControl(ARM_POWER_FULL);
+	Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS | 
+												ARM_USART_DATA_BITS_8 | 
+												ARM_USART_STOP_BITS_1 | 
+												ARM_USART_PARITY_NONE | 
+												ARM_USART_FLOW_CONTROL_NONE , 115200); 
+	
+	Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);			// réception
+	Driver_USART1.Control(ARM_USART_CONTROL_TX, 1);			// transmission
+}
+
+
+	
+	
+	
+	
+	
+
+
