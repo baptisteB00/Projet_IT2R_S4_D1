8329ad87c53a2d0fb71926584a67ff2d0342eb75
BOUSSET Baptiste <baptiste.bousset@geii2.iut-cachan.u-psud.fr>
2026-02-17 19:00:52 +0100

    code a tester encore

--- sous_fonctions/control_moteur/main.c
+++ sous_fonctions/control_moteur/main.c
@@ -23,14 +23,14 @@ void init_moteur_motricite(void){
 	LPC_SC->PCONP |= 0x00000040; // Enable PWM1
 	LPC_PINCON->PINSEL7 |= (0x11<<20)|(0x11<<18); //met les Pin P3.25 et P3.26 sur PWM1.2 et PWM 1.3
 	LPC_PWM1->PR = 0; // Prescaler
-	LPC_PWM1->MR0 = 1250; // MR0+1=25000, comptage à 1kHz
+	LPC_PWM1->MR0 = 1250; // MR0+1=1250 , comptage a 20Khz
 	LPC_PWM1->MCR |= 0x00000002; // RAZ on MR0
 	LPC_PWM1->LER |= 0x0000000F; // Autorise Modification PWM en cours de fonctionnement
-	LPC_PWM1->PCR |= 0x00000E00; // Autorise les sorties PWM1/2/3
+	LPC_PWM1->PCR |= (1<<10)|(1<<11); // Autorise les sorties PWM 2 et 3
 	LPC_PWM1->TCR |= 0x00000001; // Démarrage Timer
 
-	LPC_PWM1->MR2 = 0;
-	LPC_PWM1->MR3 = 50;
+	LPC_PWM1->MR2 = 0; // initialise la vitesse a 0
+	LPC_PWM1->MR3 = 625;// initialise la direction au milieu 
 }
 
 
@@ -62,7 +62,7 @@ void vitesse(uint8_t val_vitesse){
 
 	if(val_vitesse<=100 && val_vitesse>=0){
 	
-	LPC_PWM1->MR2 = val_vitesse * 6; //*6 car valeur entre 0;100 => 0;600
+	LPC_PWM1->MR2 = (uint16_t)(val_vitesse * 12.5); //*6 car valeur entre 0;100 => 0;600
 		
 	}
 
