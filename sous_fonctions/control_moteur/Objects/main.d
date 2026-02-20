4e13071a3ec4398484fc3f2b9db5ae66467bb495
BOUSSET Baptiste <baptiste.bousset@geii2.iut-cachan.u-psud.fr>
2026-02-20 11:59:29 +0100

    code fonctionel avec fonction pour changer l'angle des roues plus la vitesse

--- sous_fonctions/control_moteur/Objects/main.d
+++ sous_fonctions/control_moteur/Objects/main.d
@@ -1,9 +1,4 @@
-.\objects\main.o: main.c
-.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\LPC17xx.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
-.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\system_LPC17xx.h
+./objects/main.o: main.c \
+  C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\LPC17xx.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\3.0.0\Device\Include\system_LPC17xx.h
--- sous_fonctions/control_moteur/main.c
+++ sous_fonctions/control_moteur/main.c
@@ -6,31 +6,83 @@ P0.23 => moteur de motricite
 P2.25 => moteur de direction
 .......................................*/
 
+uint32_t VAL_PWM_SERVO = 50000;
 
 enum sens {AVANT = 1 , ARRIERE = -1 , STOP = 0};
+enum return_vit{vitOK,vitERROR};
 
 void init_moteur_motricite(void);
+void init_servo_moteur();
+
+enum return_vit control_vitesse(uint8_t val_vitesse);
+void direction_roue(int16_t angle);
 
 
 int main(void){
+	int i=0;
+  init_moteur_motricite();
+	
+	init_servo_moteur();
 
-init_moteur_motricite();
+	LPC_PWM1->MR2 =500;
+	
+	direction_roue(90);
+	while (1){
+		if(LPC_GPIO0->FIOPIN2){
+		direction_roue(i);
+		}
+		i++;
+		if(i>90)i=-90;
+	}
+}
+
+void init_servo_moteur()
+{
+		LPC_GPIO3->FIODIR3|=(1<<2);
+		// Validation des 4 TIMERS   
+		LPC_SC->PCONP = LPC_SC->PCONP | 0x00C00006;   
+	
+		LPC_TIM0->PR = 0;  // le registre PR prend la valeur du prescaler
+		LPC_TIM0->MR0 = 499999;    // le registre MR0 prend la valeur maximum du compteur
+		LPC_TIM0->MCR=LPC_TIM0->MCR | 0x00000003; // active le reset on mr0 et les interuptions
+	
+		LPC_TIM0->TCR = 1;  
+		
+		NVIC_SetPriority(TIMER0_IRQn,0);
+		NVIC_EnableIRQ(TIMER0_IRQn);
+}
+
+
+void TIMER0_IRQHandler(void){
+	volatile static char stat = 0 ; 
+	LPC_TIM0->IR = 1; // baissse le flag
+
+	if (stat == 1 ){
+	 LPC_TIM0->MR0 = 500000 - VAL_PWM_SERVO;
+	}
+	else
+	{
+		LPC_TIM0->MR0 = VAL_PWM_SERVO; 
+	}
+	
+	stat ^=1;
+	LPC_GPIO3->FIOPIN3 ^= (1<<2);
 	
 }
 
-void init_moteur_motricite(void){
 
-	LPC_SC->PCONP |= 0x00000040; // Enable PWM1
-	LPC_PINCON->PINSEL7 |= (0x11<<20)|(0x11<<18); //met les Pin P3.25 et P3.26 sur PWM1.2 et PWM 1.3
-	LPC_PWM1->PR = 0; // Prescaler
-	LPC_PWM1->MR0 = 1250; // MR0+1=1250 , comptage a 20Khz
-	LPC_PWM1->MCR |= 0x00000002; // RAZ on MR0
-	LPC_PWM1->LER |= 0x0000000F; // Autorise Modification PWM en cours de fonctionnement
-	LPC_PWM1->PCR |= (1<<10)|(1<<11); // Autorise les sorties PWM 2 et 3
-	LPC_PWM1->TCR |= 0x00000001; // Démarrage Timer
+void init_moteur_motricite(void)
+{
+LPC_SC->PCONP |= 0x00000040; // Enable PWM1
+LPC_PINCON->PINSEL7 |= (3<<18); // Broches P3.25
+LPC_PWM1->PR = 0; // Prescaler
+LPC_PWM1->MR0 = 2499; // MR0+1=25000, comptage à 10kHz
+LPC_PWM1->MCR |= 0x00000002; // RAZ on MR0
+LPC_PWM1->LER |= 0x0000000F; // Autorise Modification PWM en cours de fonctionnement
+LPC_PWM1->PCR |= 0x00000E00; // Autorise les sorties PWM1/2/3
+LPC_PWM1->TCR |= 0x00000001; // Démarrage Timer
+LPC_PWM1->MR2=0; // desactive la vitesse au debut 
 
-	LPC_PWM1->MR2 = 0; // initialise la vitesse a 0
-	LPC_PWM1->MR3 = 625;// initialise la direction au milieu 
 }
 
 
@@ -57,25 +109,28 @@ void direction_moteur(enum sens direction ){
 	
 }  
 
-//valeur entre 0 et 100
-void vitesse(uint8_t val_vitesse){
 
+//valeur entre 0 et 100
+enum return_vit control_vitesse(uint8_t val_vitesse){
+	
 	if(val_vitesse<=100 && val_vitesse>=0){
 	
-	LPC_PWM1->MR2 = (uint16_t)(val_vitesse * 12.5); //*6 car valeur entre 0;100 => 0;600
-		
+	LPC_PWM1->MR2 = (uint16_t)(val_vitesse * 24.99); //*6 car valeur entre 0;100 => 0;600
+		return vitOK;
+	}else {
+		LPC_PWM1->MR2 =0;
+		return vitERROR;
 	}
 
 }
 
-void direction_roue(int8_t angle){
-	uint8_t valeur_PWM;
+//angle : valeur en degrees
+void direction_roue(int16_t angle){
+
+	if ( angle <= 90 && angle >= -90){
 	
-	if ( angle < 100 && angle > -100){
-	
-		valeur_PWM = (angle+ 100)/2.0;
-		
-		LPC_PWM1->MR2 = (char) valeur_PWM;
-		
+		angle += 90; // value between 0 and 180 
+		VAL_PWM_SERVO = ((angle) * 25000.0/180.0)+25000.0; // value between 25 000 (1ms) and 50 000 (2ms)
 	}
 }
+
