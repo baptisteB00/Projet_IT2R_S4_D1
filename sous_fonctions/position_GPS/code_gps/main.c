65d0f4e469331637fb66d2d7b790fa2cf212d11b
HOUY Bryan <bryan.houy@geii2.iut-cachan.u-psud.fr>
2026-02-20 12:25:08 +0100

    Modification sur la fonction Init UART GPS

--- sous_fonctions/position_GPS/code_gps/main.c
+++ sous_fonctions/position_GPS/code_gps/main.c
@@ -1,3 +1,7 @@
+
+
+
+
 #include "stm32f4xx.h"                  // Device header
 #include "Driver_USART.h"               // CMSIS Driver:USART
 
@@ -7,10 +11,20 @@ void Init_UART_GPS(void);
 
 int main()
 {
+	char message[100];
+	
+	
 	Init_UART_GPS();
 	
+	while(1)
+	{
+		Driver_USART3.Receive(message, 100);
+		while(Driver_USART3.GetRxCount() < 100);
+		
+	}
 	
 	
+	return 0;
 }
 
 
@@ -24,4 +38,6 @@ void Init_UART_GPS(void)
 													 ARM_USART_PARITY_NONE			 |
 													 ARM_USART_FLOW_CONTROL_NONE, 
 													 115200);
+		Driver_USART3.Control(ARM_USART_CONTROL_RX, 1);			// réception
+		Driver_USART3.Control(ARM_USART_CONTROL_TX, 1);			// transmission
 	}
