6ea6e84b26ef08a45a2c630fe36675b720c626a7
HOUY Bryan <bryan.houy@geii2.iut-cachan.u-psud.fr>
2026-03-26 17:58:50 +0100

    Test

--- sous_fonctions/position_GPS/code_gps/RTE/_Target_1/RTE_Components.h
+++ sous_fonctions/position_GPS/code_gps/RTE/_Target_1/RTE_Components.h
@@ -15,6 +15,10 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
+/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.9.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+#define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+#define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
 /* Keil::CMSIS Driver:USART@2.16 */
 #define RTE_Drivers_USART1              /* Driver USART1 */
 #define RTE_Drivers_USART2              /* Driver USART2 */
