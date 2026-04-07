2376d4d5148e324fedcacbfe8f1a65f0ad58ff4b
HO Alexandre <alexandre.ho@geii2.iut-cachan.u-psud.fr>
2026-04-07 14:48:51 +0200

    c'est fini

--- sous_fonctions/pilotage_wireless/main.c
+++ sous_fonctions/pilotage_wireless/main.c
@@ -110,25 +110,12 @@ void Thread_Nunchuck (void const* argument){
 //Thread Nunchuck
 void Thread_Bluetooth (void const* argument){
 	(void)argument;
-	char data_transmit[2]; // [ Direction --> -128° to 127° , vitesse --> -128 to 127], reversible avec valeur negative
-//	while (1){
-//		if ((data[0] == 0x85)& (data[1]== 0x7C )) { // Neutre
-//			data_transmit[0] = 0x00;
-//			data_transmit[1] = 0x00;
-//		}
-//		// Vitesse
-//		if(data[1] > 0x7B){data_transmit[1] = 0x7F;}//Avance
-//		else if(data[1] < 0x7B){data_transmit[1] = 0x80;}//Recule
-//		//Direction
-//		if (((data[0] > 0x62)&(data[0] < 0x95))){data_transmit[0] = 0x00 ;} //devant
-//		else if (data[0] < 0x62){data_transmit[0] = 0x80;}// Droite
-//		else if (data[0] > 0x95){data_transmit[0] = 0x7F;}// Gauche
-//		
-//		Driver_USART1.Send(data_transmit, 2);	
-//		while(Driver_USART1.GetTxCount() < 2);		
-//		osDelay(2000);
-//	}
+	char data_transmit[3]; // [ Direction --> -128° to 127° , vitesse --> -128 to 127], reversible avec valeur negative
+
 		while (1){
+		data_transmit[2] = (data[5] & 0x01); // Lecture BP
+		data_transmit[2] = (data[5] & 0x02)>> 3; // Lecture BP
+
 		if ((data[0] == 0x85)& (data[1]== 0x7C )) { // Neutre
 			data_transmit[0] = 0x00;
 			data_transmit[1] = 0x00;
@@ -141,7 +128,7 @@ void Thread_Bluetooth (void const* argument){
 		else if (data[0] < 0x62){data_transmit[0] = 0x80;}// Droite
 		else if (data[0] > 0x95){data_transmit[0] = 0x7F;}// Gauche
 		
-		Driver_USART1.Send(data_transmit, 2);	
+		Driver_USART1.Send(data_transmit,3);	
 		while(Driver_USART1.GetTxCount() < 2);		
 		osDelay(2000);
 	}
